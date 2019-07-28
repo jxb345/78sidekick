8dd10f75162d8d3b1d393b2cb69ab482f4ed8a67
Jesse Bell <jkurtasbell@gmail.com>
2019-07-28 12:17:06 -0700

    Alphabatize Dropdown Menu For Genres Form

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -49,28 +49,28 @@ class Form extends React.Component {
           {/* <input type="text" name="genre" value={this.props.genre} onChange={this.props.handleChange}></input> */}
           <select value={this.props.genre} onChange={this.props.handleChangeGenre}>
             <option value="default" defaultValue>Select Genre</option>
-            <option value="popular-music">Popular Music</option>
+            {/* <option value="acoustic">Acoustic</option> */}
+            <option value="blues">Blues</option>
+            <option value="christmas">Christmas</option>
+            <option value="comedy">Comedy</option>
+            <option value="country" >Country</option>
+            <option value="cowboy">Cowboy</option>
+            <option value="dance" >Dance</option>
+            <option value="ethnic">Ethnic</option>
+            <option value="film">Film</option>
+            <option value="folk">Folk</option>
+            <option value="gospel" >Gospel</option>
+            <option value="hawaiian">Hawaiian</option>
+            <option value="hillbilly">Hillbilly</option>
             <option value="instrumental" >Instrumental</option>
             <option value="jazz">Jazz</option>
-            {/* <option value="acoustic">Acoustic</option> */}
-            <option value="vocal" >Vocal</option>
             <option value="latin">Latin</option>
-            <option value="blues">Blues</option>
-            <option value="country" >Country</option>
-            <option value="polka">Polka</option>
-            <option value="hillbilly">Hillbilly</option>
-            <option value="dance" >Dance</option>
-            <option value="swing">Swing</option>
-            <option value="ethnic">Ethnic</option>
-            <option value="folk">Folk</option>
             <option value="novelty">Novelty</option>
-            <option value="comedy">Comedy</option>
             <option value="orchestral" >Orchestral</option>
-            <option value="hawaiian">Hawaiian</option>
-            <option value="film">Film</option>
-            <option value="christmas">Christmas</option>
-            <option value="gospel" >Gospel</option>
-            <option value="cowboy">Cowboy</option>
+            <option value="polka">Polka</option>
+            <option value="popular-music">Popular Music</option>
+            <option value="swing">Swing</option>
+            <option value="vocal" >Vocal</option>
           </select>
         </div>
         <button className="play-button">PLAY</button>
