3bf96f68f8b39c54c7aa690e9be4398a31563fdd
Jesse Bell <jkurtasbell@gmail.com>
2019-07-28 10:37:03 -0700

    Check Character Length of Artist And Song And Shorten To Fit In Metadata

--- client/src/components/MetaData.jsx
+++ client/src/components/MetaData.jsx
@@ -7,6 +7,8 @@ class MetaData extends React.Component {
 
 
   render () {
+    let title = `${this.props.title}`;
+    let artist = `${this.props.artist}`;
     const spanStyle = {
       fontSize: 15,
     }
@@ -15,17 +17,26 @@ class MetaData extends React.Component {
     if (`${this.props.year}` !== '') {
       year = `(19` +`${this.props.year}` + `)`;
     }
-    if (this.props.title === '' && this.props.artist === '') {
+    if (title === '' && artist === '') {
       return <div>
         </div>
     } else {
+      console.log('title.length', title.length);
+      if (title.length > 30) {
+        title = title.slice(0, 30) + '...';
+      }
+      if (artist.length > 25) {
+        artist = artist.slice(0, 25) + '...';
+      }
       return (
         // displays fetched metadata for a given song
+        // need to limit song title if it is longer
+        // than X number of characters
         <div>
           <a href={detailsPage} target="_blank">
-          <span>"{this.props.title}"</span>
+          <span>"{title}"</span>
           <br></br>
-         <span style={spanStyle} >{this.props.artist}</span>
+         <span style={spanStyle} >{artist}</span>
          <br></br>
          <span style={spanStyle}>{year}</span>
          </a>
