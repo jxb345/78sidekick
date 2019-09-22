fde85ca215c955ce3596179eedcaa017d6c865f5
Jesse Bell <jkurtasbell@gmail.com>
2019-09-22 13:50:38 -0700

    Add Text To Info Component

--- client/src/components/Info.jsx
+++ client/src/components/Info.jsx
@@ -9,10 +9,20 @@ class Info extends React.Component {
     return (
       <div>
         <div>
-            <h3>What</h3>
+          <h3>What</h3>
+            <p>
+              sideKick 78 streams
+            </p>
 
-            <h3>How</h3>
-            <h3>Who</h3>
+          <h3>How</h3>
+            <p>
+              sideKick 78 uses the <a href="https://archive.org/">Internet Archive</a> API to search the <a href="https://archive.org/details/georgeblood">George Blood collection</a> of digitized 78s.<br></br>  This digitization effort was generously funded by the Kahle-Austin Foundation. <br></br>The collection contains over 100,000 items. <br></br>Enjoy!';
+            </p>
+          <h3>Who</h3>
+          <p>
+            sideKick 78 is by Jesse Bell with contributions from Scott.
+          </p>
+
 
         </div>
 
